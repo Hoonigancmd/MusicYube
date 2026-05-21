b77a041a95097dbe7ee409e4f7ce95c9851ca6dd
Radingka-UB <radingkaarfian@student.ub.ac.id>
2026-05-21 19:34:38 +0700

    Rename Playlist class to MusicUI and update references accordingly

--- src/view/MusicUI.java
+++ src/view/MusicUI.java
@@ -2,18 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package musicyube;
+package view;
 
 /**
  *
  * @author TokDeLokTo
  */
-public class Playlist extends javax.swing.JFrame {
+public class MusicUI extends javax.swing.JFrame {
 
     /**
      * Creates new form Playlist
      */
-    public Playlist() {
+    public MusicUI() {
         initComponents();
     }
 
@@ -173,20 +173,20 @@ public class Playlist extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Playlist.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MusicUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Playlist.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MusicUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Playlist.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MusicUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Playlist.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MusicUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Playlist().setVisible(true);
+                new MusicUI().setVisible(true);
             }
         });
     }
