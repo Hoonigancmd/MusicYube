cfa5b70e97156f2591160e80a4e920557d3b4f1d
Radingka-UB <radingkaarfian@student.ub.ac.id>
2026-05-21 21:25:07 +0700

    Remove unused mainFrame variable from AppNavigator and update location setting for UI components

--- src/controller/AppNavigator.java
+++ src/controller/AppNavigator.java
@@ -1,26 +1,21 @@
 package controller;
 
-import javax.swing.JFrame;
 import view.*;
 import model.*;
 
 public class AppNavigator {
-    private JFrame mainFrame;
-    public AppNavigator(){
-        mainFrame=new JFrame();
-    }
     public void start(){
         viewLoginUI();
     }
     public void viewLoginUI(){
         LoginUI loginUI =new LoginUI(this);
         loginUI.setVisible(true);
-        loginUI.setLocationRelativeTo(mainFrame);
+        loginUI.setLocationRelativeTo(null);
     }
     public void viewMusicUI(User user){
         MusicUI musicUI =new MusicUI(user, this);
         musicUI.setVisible(true);
-        musicUI.setLocationRelativeTo(mainFrame);
+        musicUI.setLocationRelativeTo(null);
 
     }
 }
