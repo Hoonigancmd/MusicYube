5a9d61ae1eaea04821573eb90ee600c494e79793
Radingka-UB <radingkaarfian@student.ub.ac.id>
2026-05-21 20:12:18 +0700

    Refactor main method to launch MusicUI instead of Playlist; update MusicUI class name and package declaration for consistency

--- src/model/User.java
+++ src/model/User.java
@@ -14,46 +14,6 @@ public class User {
         this.password = password;
         this.id = id;
         this.playlist = new ArrayList<>();
-<<<<<<< HEAD
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public List<FileMusik> getPlaylist() {
-        return new ArrayList<>(playlist);
-    }
-
-    public void setPlaylist(List<FileMusik> playlist) {
-        if (playlist != null) {
-            this.playlist = new ArrayList<>(playlist);
-        }
-    }
-
-    public void addToFileMusik(FileMusik fileMusik) {
-        if (fileMusik != null && !playlist.contains(fileMusik)) {
-            playlist.add(fileMusik);
-        }
-    }
-
-    public void removeFromFileMusik(FileMusik fileMusik) {
-        playlist.remove(fileMusik);
-    }
-
-    @Override
-    public String toString() {
-        return username;
-=======
->>>>>>> 760ee9db77990b014ea533457376a73f88d29214
     }
 
     public String getUsername() {
