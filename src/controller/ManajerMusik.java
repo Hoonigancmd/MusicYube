e0cca10e59365ea4ff2eb3902dce7a898ced5fbc
Radingka-UB <radingkaarfian@student.ub.ac.id>
2026-05-21 15:19:08 +0700

    Implement ManajerMusik class with PlayOrPause enum and initial methods; add User model class

---
+++ src/controller/ManajerMusik.java
@@ -0,0 +1,24 @@
+package controller;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.Clip;
+
+import model.User;
+public class ManajerMusik{
+    public enum PlayOrPause{
+        PLAY,
+        PAUSE
+    }
+    private User user;
+    private AudioInputStream aiStream;
+    private Clip clip;
+    public ManajerMusik(User user){
+        this.user=user;
+        aiStream=new AudioInputStream(user.getPlaylist().get(0).getFile().getPath());
+    }
+    public boolean isMemutar(){
+        
+    }
+    public void setPlayOrPause(PlayOrPause playOrPause){
+
+    }
+}
